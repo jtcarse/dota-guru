aa4c8a695c488e9b9beeb76ecc6a8acbf68396f6
John Carse <jtcarse@gmail.com>
2019-04-04 00:19:19 -0500

    started rewriting crawler.

--- API.py
+++ API.py
@@ -1,5 +1,11 @@
 import requests
 
+def setup_api():
+    import auth
+    api = API(auth.key, test=False)
+
+    return api
+
 class API:
     def __init__(self, key, test=True):
         self.key = key
@@ -20,10 +26,10 @@ class API:
 
         if not r.status_code == 200:
             print("Request error: {}".format(r.status_code))
-            return ""
+            return None
         else:
             print("Success!")
-            return r.json()["result"]
+            return r.json()["result"]["heroes"]
 
     def get_items(self):
         url = "http://api.steampowered.com/IEconDOTA2_{}/GetGameItems/v1".format(self.id)
@@ -36,15 +42,15 @@ class API:
 
         if not r.status_code == 200:
             print("Request error: {}".format(r.status_code))
-            return ""
+            return None
         else:
             print("Success!")
-            return r.json()["result"]
+            return r.json()["result"]["items"]
 
     def vanity_to_id(self, vanity, bit=32):
         if not (bit == 64 or bit == 32):
             print("Error: id must be 32-bit or 64-bit")
-            return ""
+            return None
 
         url = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/"
         params = {
@@ -56,7 +62,7 @@ class API:
 
         if not r.status_code == 200:
             print("Request error: {}".format(r.status_code))
-            return ""
+            return None
         else:
             if r.json()["response"]["success"] == 1:
                 if bit == 64:
@@ -65,7 +71,7 @@ class API:
                     return str(int(r.json()["response"]["steamid"]) - 76561197960265728)
             else:
                 print("API error: {}".format(r.json()["response"]["message"]))
-                return ""
+                return None
 
     def get_match_history(self, matches=25, user_id=None, before_id=None, hero_id=None):
         url = "http://api.steampowered.com/IDOTA2Match_{}/GetMatchHistory/v1".format(self.id)
@@ -87,10 +93,10 @@ class API:
 
         if not r.status_code == 200:
             print("Request error: {}".format(r.status_code))
-            return ""
+            return None
         else:
             print("Success!")
-            return r.json()["result"]
+            return r.json()["result"]["matches"]
 
     def get_match_details(self, match_id):
         url = "http://api.steampowered.com/IDOTA2Match_{}/GetMatchDetails/v1".format(self.id)
@@ -103,7 +109,7 @@ class API:
 
         if not r.status_code == 200:
             print("Request error: {}".format(r.status_code))
-            return ""
+            return None
         else:
             print("Success!")
             return r.json()["result"]
--- crawler.py
+++ crawler.py
@@ -1,73 +1,22 @@
 import auth
-from API import API
+from API import setup_api
 import sqlite3
 import time
 
 class Crawler:
     def __init__(self):
-        self.api = API(auth.key, test=False)
+        self.api = setup_api()
 
-    def crawl(self, vanity):
-        connection = sqlite3.connect("{}.db".format(vanity))
+    def store_matches(self, num_matches):
+        connection = sqlite3.connect("dota-guru.db")
         c = connection.cursor()
 
-        user_id = self.api-vanity_to_id(vanity)
-        time.sleep(1)
+        t = time.time()
+        matches = api.get_match_history(matches=100)
 
-        result = api.get_match_history(user_id, matches=100)
-        time.sleep(1)
-        matches = result["matches"]
+        # parse out match_ids (and validation info?)
 
-        match_ids = []
-        heroes = []
-        teammates = []
-        enemies = []
-
-        for match in matches:
-            match_ids.append(match["match_id"])
-            is_radiant = True
-            radiant = []
-            dire = []
-
-            """FUCKED UP"""
-            for player in match["players"]:
-                if player["player_slot"] < 5:
-                    radiant.append(player["hero_id"])
-                else:
-                    dire.append(player["hero_id"])
-                    if player["account_id"] == user_id:
-                        is_radiant = False
-
-            if is_radiant:
-                teammates += radiant
-
-
-
-        c.execute(
-        """
-            CREATE TABLE matches (
-                match_id integer PRIMARY KEY,
-                hero integer,
-                teammate_1 integer,
-                teammate_2 integer,
-                teammate_3 integer,
-                teammate_4 integer,
-                enemy_1 integer,
-                enemy_2 integer,
-                enemy_3 integer,
-                enemy_4 integer,
-                enemy_5 integer,
-                win integer
-            )
-        """
-        )
-
-        c.executemany(
-        """
-            INSERT INTO matches VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        """,
-
-        )
+        # store them in the table "matches"
 
         connection.commit()
         connection.close()
